9ce61b792863db1b2da3a85f085df9f8154340f2
testlunar <diana.umudova@gmail.com>
2022-12-15 18:40:58 +0300

    тест на заполнение всех полей

--- src/test/java/EmailTests.java
+++ src/test/java/EmailTests.java
@@ -14,11 +14,12 @@ public class EmailTests {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 30000;
     }
 
 
     @Test
-    void wrongEmailTest(){
+    void wrongEmailTest() {
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         executeJavaScript("$('#fixedban').remove()");
@@ -43,6 +44,39 @@ public class EmailTests {
         $("#submit").scrollIntoView(true).click();
         $("#example-modal-sizes-title-lg").shouldBe(exist);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+    }
+
+
+    @Test
+    void fillLinesTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue("Di");
+        $("#lastName").setValue("Ev");
+        $("#userEmail").setValue("di@maimail.ru");
+        $("#genterWrapper").$(byText("Female")).click(); // best
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").selectOption("1988");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#currentAddress").setValue("address 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Moscow")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Di"), text("Ev"),
+                text("di@maimail.ru"), text("1234567890"));
 
     }
 }
