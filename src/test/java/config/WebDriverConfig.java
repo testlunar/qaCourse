1ce69924cd97dd488b320f62c9e7ffb76ef13a67
testlunar <diana.umudova@gmail.com>
2023-03-07 18:42:34 +0300

    добавены настройки конфигураций для запуска локально и удаленно

---
+++ src/test/java/config/WebDriverConfig.java
@@ -0,0 +1,31 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:${env}.properties"
+})
+
+public interface WebDriverConfig extends Config {
+
+    @Key("baseUrl")
+    @DefaultValue("https://demoqa.com")
+    String getBaseUrl();
+
+    @Key("remoteUrl")
+    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
+    String getRemoteUrl();
+
+    @Key("browserName")
+    @DefaultValue("chrome")
+    String getBrowserName();
+
+    @Key("browserVersion")
+    @DefaultValue("100.0")
+    String getBrowserVersion();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String getBrowserSize();
+
+}
